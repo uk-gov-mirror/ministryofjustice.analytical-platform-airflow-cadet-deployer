1b20a202329e4cb86b6b0ae0b667be5b965d6c5f
Andrew Craik <andrew.craik@justice.gov.uk>
2026-08-20 09:19:26 +0100

    refactor to get round linting error

--- src/create-a-derived-table.sh
+++ src/create-a-derived-table.sh
@@ -207,9 +207,9 @@ function set_dual_materialization_env_vars() {
   if [ "$CHECK_DUAL_MATERIALIZATION" = "True" ]; then
     echo "Checking for models with dual materialization config"
 
-    while IFS= read -r variable; do
-      export "$variable"
-      echo "Added: $variable"
+    while IFS='=' read -r name value; do
+      export "$name=$value"
+      echo "Added: $name=$value"
     done < <(
       dbt run-operation check_if_models_exist_by_tag \
         --args '{"tag_names":["dual_materialization"], "tag_mode":"intersect"}' \
